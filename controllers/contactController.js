522a05d93d7a54d2c8b0792fe319f478e5d390d6
swataswayam-14 <124362279+swataswayam-14@users.noreply.github.com>
2024-01-07 14:24:02 +0530

    Add files via upload

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler")
 const Contact = require("../models/contactModel")
 
 const getContacts = asyncHandler(async(req,res) =>{
-    const contacts = await Contact.find()
+    const contacts = await Contact.find({user_id: req.user.id})
     res.status(200).json(contacts)
 })
 const getContact = asyncHandler(async(req,res) =>{
@@ -19,6 +19,10 @@ const updateContact = asyncHandler(async(req,res) =>{
         res.status(404)
         throw new Error("Contact not found")
     }
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403)
+        throw new Error("User don't have permission to update other user contacts")
+    }
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
@@ -32,7 +36,11 @@ const deleteContact = asyncHandler(async(req,res) =>{
         res.status(404)
         throw new Error("Contact not found")
     }
-    await Contact.findByIdAndDelete()
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403)
+        throw new Error("User don't have permission to delete other user contacts")
+    }
+    await Contact.deleteOne({_id:req.params.id})
     res.status(200).json(contact)
 })
 const createContact = asyncHandler(async(req,res) =>{
@@ -46,6 +54,7 @@ const createContact = asyncHandler(async(req,res) =>{
         name,
         email,
         phone,
+        user_id: req.user.id
     })
     res.status(201).json(contact)
    
